124a4b75d91d27e091f8a066e3ca5ad08d99f471
loginshin <95666408+loginshin@users.noreply.github.com>
2023-08-04 17:32:17 +0900

    feat)back) fix inital setting
    just connecting mariaDB and gradle setting

---
+++ smClubBack/src/main/java/com/smClub/smClub/SmClubApplication.java
@@ -0,0 +1,13 @@
+package com.smClub.smClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmClubApplication.class, args);
+	}
+
+}
