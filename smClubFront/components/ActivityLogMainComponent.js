3f40b60ab96e5f212a98d470fbd56213bc856c93
yeonji123 <duathshdwkd1@naver.com>
2023-08-06 20:13:21 +0900

    📝Produce) generate ActivityLogMainComponent
    ActivityLogMainComponent is the Activity Log component on the main page.
    this is a new file!

---
+++ smClubFront/components/ActivityLogMainComponent.js
@@ -0,0 +1,15 @@
+// 메인페이지에 있는 활동 일지 컴포넌트
+import React from 'react';
+import {
+    View
+} from 'react-native';
+
+const ActivityLogMainComponent = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+export default ActivityLogMainComponent;
