e0e462c6bfa4b6f0e4a98cac1cb161574b80e1f5
yeonji123 <duathshdwkd1@naver.com>
2023-08-06 23:49:12 +0900

    ✨feat) add axios code in mainscreen

--- smClubFront/screens/Main/MainScreen.js
+++ smClubFront/screens/Main/MainScreen.js
@@ -1,10 +1,14 @@
 // 메인페이지에서 동아리, 모집 공고 슬라이드 하는 페이지
-import React, { useState, useEffect } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import {
-    View, Text, Dimensions
+    View, Text, Button,
 } from 'react-native';
+
 // npm install react-native-tab-view 
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+
+
 
 // ------------Components
 import ClubTypeAndActivityLogScreenComponent from '../../components/ClubTypeAndActivityLogScreenComponent';
@@ -13,7 +17,6 @@ import AnnouncementListScreenComponent from '../../components/AnnouncementListSc
 
 // ------------styles
 import styles from '../../components/Style';
-
 
 
 // 컴포넌트
@@ -34,7 +37,7 @@ const renderTabBar = props => (
     <TabBar
         {...props}
         indicatorStyle={{ backgroundColor: 'gray' }} // 탭바 밑줄 색깔
-        style={styles.tabbarstyle} 
+        style={styles.tabbarstyle}
         renderLabel={({ route, focused, color }) => ( // 탭바 글씨 텍스트 값, 색깔(activiColor)
             <Text style={[styles.tabBarText, { color }]}>
                 {route.title}
@@ -46,15 +49,63 @@ const renderTabBar = props => (
 );
 
 const MainScreen = (props) => {
-    
+    const [] = useState([]); // 동아리 분과 리스트
+    const [] = useState([]); // 동아리 활동 일지 리스트
+
     const [index, setIndex] = useState(0);
     const [routes] = useState([
         { key: 'first', title: '동아리' },
         { key: 'second', title: '모집공고' },
     ]);
 
+    // ---------------서버 통신 -----------
+    // controller 모두 설계 완료 되면 request 데이터와 response 데이터 설계
+    const reqestData = async () => {
+        console.log("요청을 보냅니다")
+        const object = {
+            studentId: "2017225002",
+            userName: "신정환",
+            userId: "loginshin",
+            userPw: "4865",
+            img: "https://loginshin.tistory.com/manage/setting/blog",
+            major: "computer",
+            phoneNumber: "01040510673",
+            email: "shinwjdghks@gmail.com",
+            userAuthor: "srv001",
+            notification: true,
+            userAgree: true,
+            userAgreeDate: "2023-08-06",
+            createDate: "2023-07-07",
+            updateDate: "2023-08-03"
+        }
 
 
+        await axios({
+            method: "post",
+            url: "http://192.168.0.49:8080/test",
+            header: {
+                "content-type": "application/json",
+            },
+            data: object
+        })
+            // 받오는 데이터
+            .then((res) => {
+                console.log("res data를 받았습니다", res.data);
+            })
+            .catch((err) => {
+                console.log("데이터를 보내는데 실패했습니다")
+            });
+    }
+
+
+
+
+    // //데이터 이동 테스트 코드
+    useEffect(()=> {
+        console.log('MainScreen');
+        //앱 실행하면 화면에 오면 서버에서 동아리 분과 리스트, 동아리 활동 일지 리스트 가져오기
+        //reqestData();
+    },[])
 
     return (
         <View style={styles.container}>
