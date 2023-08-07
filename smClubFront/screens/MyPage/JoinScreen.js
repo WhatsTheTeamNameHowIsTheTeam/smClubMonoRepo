50fd34fe033eb507ae80d24760a2d7bcbfe995ff
shyuni <60885427+sehyun00@users.noreply.github.com>
2023-08-07 20:03:11 +0900

    ✨Feat) JoinScreen
    거의 다 끝남

--- smClubFront/screens/MyPage/JoinScreen.js
+++ smClubFront/screens/MyPage/JoinScreen.js
@@ -1,8 +1,64 @@
 // 회원가입
-import React from 'react';
-import { View, SafeAreaView, Text, StyleSheet, TextInput, ScrollView } from 'react-native';
+// 모듈
+import Checkbox from 'expo-checkbox';
+import React, { useState } from 'react';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    ScrollView,
+    Alert,
+    StyleSheet
+} from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
 
 const JoinScreen = () => {
+
+    const [idTextInput, setIdTextInput] = useState(""); //  ID
+    const [pwTextInput, setPwTextInput] = useState(""); //  PW
+    const [pwConfirmTextInput, setPwConfirmTextInput] = useState(""); //  PWConfirm
+    const [nameTextInput, setNameTextInput] = useState(""); //  이름
+    const [studentIdTextInput, setStudentIdTextInput] = useState(""); //  학번
+    const [majorTextInput, setmajorTextInput] = useState(""); //  전공
+    const [contactTextInput, setContactTextInput] = useState(""); //  연락처
+    const [termsAgreed, setTermsAgreed] = useState(false);
+
+    const majors = [
+        '전공을 선택해주세요',
+        '컴퓨터공학',
+        '전자공학',
+        '경영학',
+        '사회학',
+        '컴퓨터공학',
+        '전자공학',
+        '경영학',
+        '사회학',
+    ];
+
+    const toggleMajorSelection = (index, value) => {
+        setmajorTextInput(value);
+    };
+
+    const handleSubmit = () => {
+        if (!idTextInput || !pwTextInput || !pwConfirmTextInput ||
+            !nameTextInput || !studentIdTextInput || !majorTextInput || !contactTextInput) {
+            Alert.alert('오류', '모든 필드를 기입해주세요.');
+            return;
+        }
+
+        if (pwTextInput !== pwConfirmTextInput) {
+            Alert.alert('오류', '비밀번호와 비밀번호 확인란이 일치하지 않습니다.');
+            return;
+        }
+
+        if (!termsAgreed) {
+            Alert.alert('오류', '이용약관에 동의해주세요.');
+            return;
+        }
+    };
+
     return (
         // 회원정보 입력
         <SafeAreaView style={styles.container}>
@@ -11,57 +67,73 @@ const JoinScreen = () => {
 
                     <TextInput
                         style={styles.input}
+                        onChangeText={setIdTextInput}
                         placeholder="아이디"
                     />
 
                     <TextInput
                         style={styles.input}
+                        onChangeText={setPwTextInput}
                         placeholder="비밀번호"
+                        secureTextEntry
                     />
-
 
                     <TextInput
                         style={styles.input}
+                        onChangeText={setPwConfirmTextInput}
                         placeholder="비밀번호 확인"
+                        secureTextEntry
                     />
-
 
                     <TextInput
                         style={styles.input}
+                        onChangeText={setNameTextInput}
                         placeholder="이름"
                     />
 
-
                     <TextInput
                         style={styles.input}
+                        onChangeText={setStudentIdTextInput}
                         placeholder="학번"
+                        keyboardType="numeric"
                     />
 
+                    <ModalDropdown
+                        options={majors}
+                        defaultValue={majors[0]}
+                        onSelect={toggleMajorSelection}
+                        style={styles.input}
+                        textStyle={styles.inputText}
+                        dropdownTextStyle={styles.inputText}
+                    />
 
                     <TextInput
                         style={styles.input}
-                        placeholder="학과"
-                    />
-
-
-                    <TextInput
-                        style={styles.input}
-                        placeholder="전공"
-                    />
-                    
-                    <TextInput
-                        style={styles.input}
+                        onChangeText={setContactTextInput}
                         placeholder="연락처"
-                    />
-                    
-                    <TextInput
-                        style={styles.input}
-                        placeholder="이용약관 동의"
+                        keyboardType="phone-pad"
                     />
 
+                    <View style={styles.input}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text>이용약관에 동의합니다.</Text>
+                            <Checkbox style={styles.checkbox}
+                                // disabled 
+                                value={termsAgreed}
+                                onValueChange={setTermsAgreed}
+                            />
+                        </View>
+                    </View>
 
+                </View >
 
-                </View ></ScrollView>
+                <TouchableOpacity
+                    onPress={handleSubmit}
+                    style={styles.signUp}
+                >
+                    <Text style={{ color: 'white' }}>회원가입</Text>
+                </TouchableOpacity>
+            </ScrollView>
 
         </SafeAreaView>
     );
@@ -87,6 +159,21 @@ const styles = StyleSheet.create({
         marginHorizontal: 30,
         marginTop: 30,
         paddingHorizontal: 20,
+        justifyContent: 'center',
+    },
+    inputText: {
+        fontSize: 14,
+    },
+    checkbox: {
+        margin: 8,
+    },
+    signUp: {
+        backgroundColor: 'blue',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 10,
+        marginHorizontal: 100,
+        marginTop: 30,
     },
 });
 export default JoinScreen;
