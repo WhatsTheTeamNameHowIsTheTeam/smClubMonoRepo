951129a3c1b512bbfef735a54ba6f1cfbd399c27
shyuni <60885427+sehyun00@users.noreply.github.com>
2023-08-07 16:19:25 +0900

    ✨Feat) MyPageScreen

--- smClubFront/components/Style.js
+++ smClubFront/components/Style.js
@@ -2,12 +2,42 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#fff',
-        alignItems:'center',
-        justifyContent:'center',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
 
-    }
+    },
+    // 로그인 페이지    ====================
+    login_container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingLeft: '10%',
+        paddingRight: '10%',
+        justifyContent: 'center',
+    },
+    login_formArea: {
+        width: '100%',
+        paddingBottom: '10%',
+    },
+    login_textForm: {
+        borderWidth: 0.5,
+        borderColor: '#888',
+        width: '100%',
+        height: '5%',
+        paddingLeft: 5,
+        paddingRight: 5,
+        marginBottom: 5,
+    },
+    login_buttonArea: {
+        marginTop: 5,
+    },
+    login_link: {
+        fontWeight: 'bold',
+        color: 'green',
+    },
+
+    
 });
 export default styles;
--- smClubFront/screens/MyPage/MyPageScreen.js
+++ smClubFront/screens/MyPage/MyPageScreen.js
@@ -1,13 +1,158 @@
 //마이페이지
+// 모듈
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const MyPageScreen = (props) => {
+// 파일
+import styles from '../../components/Style'
+
+const MyPageScreen = ({ navigation }) => {
+
+    //탈퇴 확인
+    const secessionButtonPress = () => {
+        Alert.alert(' 탈퇴하실건가요?', '동아리 가입 내역이 전부 사라집니다.', [
+            {
+                text: '취소',
+                style: 'cancel',
+            },
+            { text: '확인' },
+        ]);
+    }
+
+    //로그아웃 확인
+    const logoutButtonPress = () => {
+        Alert.alert(' 로그아웃 하실건가요?', '', [
+            {
+                text: '취소',
+                style: 'cancel',
+            },
+            { text: '확인', onPress: () => { logout(); } },
+        ]);
+    }
+
+    // 로그아웃
+    const logout = async () => {
+        try {
+            AsyncStorage.clear();
+            navigation.navigate('LoginScreen'); 
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <View>
-            
+        <View style={styles.login_container}>
+
+            {/* 내정보 */}
+            <View style={MyPagestyles.myInfo} >
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    marginRight: 10
+                }}>
+                    <TouchableOpacity>
+                        <View style={MyPagestyles.profile}></View>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{
+                    flexDirection: 'column',
+                    flex: 2,
+                    justifyContent: 'center'
+                }}>
+                    <Text style={{ fontSize: 20, marginBottom: 8, }}>김세현</Text>
+                    <Text style={{ marginBottom: 5, }}>신소재공학과</Text>
+                    <Text>010-2232-2920</Text>
+                </View>
+            </View>
+
+            {/* 약관 및 설정 */}
+            <View style={{ flex: 2.5 }}>
+
+                <TouchableOpacity>
+                    <View style={MyPagestyles.setup}>
+                        <Text style={MyPagestyles.setupText} >MY 동아리</Text>
+                        <View style={MyPagestyles.separator} />
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <View style={MyPagestyles.setup}>
+                        <Text style={MyPagestyles.setupText}>MY 동아리 활동 일지</Text>
+                        <View style={MyPagestyles.separator} />
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <View style={MyPagestyles.setup}>
+                        <Text style={MyPagestyles.setupText}>동아리 가입 신청 내역</Text>
+                        <View style={MyPagestyles.separator} />
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <View style={MyPagestyles.setup}>
+                        <Text style={MyPagestyles.setupText}>약관 및 정책</Text>
+                        <View style={MyPagestyles.separator} />
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <View style={MyPagestyles.setup}>
+                        <Text
+                            style={MyPagestyles.setupText}
+                            onPress={secessionButtonPress}
+                        >
+                            어플 탈퇴하기
+                        </Text>
+                        <View style={MyPagestyles.separator} />
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <View style={MyPagestyles.setup}>
+                        <Text
+                            style={MyPagestyles.setupText}
+                            onPress={logoutButtonPress}
+                        >
+                            로그아웃
+                        </Text>
+                        <View style={MyPagestyles.separator} />
+                    </View>
+                </TouchableOpacity>
+            </View>
+
         </View>
     );
 };
+const MyPagestyles = StyleSheet.create({
+    myInfo: {
+        backgroundColor: '#D9D9D9',
+        borderRadius: 10,
+        alignContent: 'center',
+        flexDirection: 'row',
+        marginBottom: 20,
+        padding: '7%',
+    },
+    profile: {
+        backgroundColor: 'white',
+        width: 90,
+        height: 90,
+        borderRadius: 10,
+    },
+    separator: {
+        borderBottomColor: 'black',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    setup: {
+        width: '100%',
+        marginTop: 15,
+        justifyContent: 'flex-start',
+    },
+    setupText: {
+        fontSize: 30,
+    },
+});
 
 export default MyPageScreen;
