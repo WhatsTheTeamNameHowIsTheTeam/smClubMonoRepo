c6a6b945839ed2b1e6b64dd24ac955664aa49450
shyuni <60885427+sehyun00@users.noreply.github.com>
2023-08-07 17:33:40 +0900

    ✨Feat) Feat JoinScreen

--- smClubFront/screens/MyPage/JoinScreen.js
+++ smClubFront/screens/MyPage/JoinScreen.js
@@ -1,13 +1,92 @@
 // 회원가입
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView, Text, StyleSheet, TextInput, ScrollView } from 'react-native';
 
 const JoinScreen = () => {
     return (
-        <View>
+        // 회원정보 입력
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+                <View>
 
-        </View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="아이디"
+                    />
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="비밀번호"
+                    />
+
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="비밀번호 확인"
+                    />
+
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="이름"
+                    />
+
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="학번"
+                    />
+
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="학과"
+                    />
+
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder="전공"
+                    />
+                    
+                    <TextInput
+                        style={styles.input}
+                        placeholder="연락처"
+                    />
+                    
+                    <TextInput
+                        style={styles.input}
+                        placeholder="이용약관 동의"
+                    />
+
+
+
+                </View ></ScrollView>
+
+        </SafeAreaView>
     );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignContent: 'center',
+        backgroundColor: 'white'
+    },
+    scrollView: {
+        backgroundColor: '#D9D9D9',
+        marginHorizontal: 20,
+        marginVertical: 20,
+        borderRadius: 10,
+    },
+    input: {
+        height: 55,
+        borderRadius: 10,
+        backgroundColor: 'white',
+        marginHorizontal: 30,
+        marginTop: 30,
+        paddingHorizontal: 20,
+    },
+});
 export default JoinScreen;
