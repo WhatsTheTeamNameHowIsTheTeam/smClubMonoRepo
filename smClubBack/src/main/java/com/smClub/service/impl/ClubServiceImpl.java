224e03eef63218fc46538633f8424d84fe422294
seohansoo <122590342+seohansoo@users.noreply.github.com>
2023-08-11 04:17:27 +0900

    feat)back) created service interface and class files

--- smClubBack/src/main/java/com/smClub/service/impl/ClubServiceImpl.java
+++ smClubBack/src/main/java/com/smClub/service/impl/ClubServiceImpl.java
@@ -1,6 +1,8 @@
 
 
-package com.smClub.service;
+package com.smClub.service.impl;
+
+import com.smClub.service.ClubService;
 
 public class ClubServiceImpl implements ClubService {
     public ClubServiceImpl() {
---
+++ smClubBack/src/main/java/com/smClub/service/impl/QuestionServiceImpl.java
@@ -0,0 +1,6 @@
+package com.smClub.service.impl;
+
+import com.smClub.service.QuestionService;
+
+public class QuestionServiceImpl implements QuestionService {
+}
