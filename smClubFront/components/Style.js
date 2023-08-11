9f50db48207f9135c7876aaa8945ddfd66a04d40
yeonji123 <duathshdwkd1@naver.com>
2023-08-11 15:27:01 +0900

    💄Design) component design

--- smClubFront/components/Style.js
+++ smClubFront/components/Style.js
@@ -3,57 +3,57 @@ import { StyleSheet, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#fff',
-        width:Dimensions.get('window').width,
-        height:Dimensions.get('window').height,
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
 
     // ----------------MainScreen
-    tabbarstyle:{ // 탭바 스타일
-        backgroundColor:'white',
+    tabbarstyle: { // 탭바 스타일
+        backgroundColor: 'white',
     },
-    tabBarText:{
-        margin: 8, 
-        fontSize:20, 
-        fontWeight:'bold',
+    tabBarText: {
+        margin: 8,
+        fontSize: 20,
+        fontWeight: 'bold',
     },
 
 
 
     // ----------------ClubTypeComponent 동아리 분과 컴포넌트
-    clubTypeTouchable:{
+    clubTypeTouchable: {
         width: width * 0.28,
-        height : width * 0.31,
-        alignItems:'center',
-        justifyContent:'center',
+        height: width * 0.31,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    clubTypeView :{ 
+    clubTypeView: {
         width: '80%',
         height: '100%',
-        justifyContent:'center',
-        alignItems:'center',
-        padding:5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 5,
     },
-    clubTypeImageView:{ // 정사각형으로 만들기 위해
-        justifyContent:'space-around',
-        alignItems:'center',
-        width : '100%',
+    clubTypeImageView: { // 정사각형으로 만들기 위해
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        width: '100%',
         height: '70%',
-        backgroundColor:'#D9D9D9',
-        borderRadius:15,
+        backgroundColor: '#D9D9D9',
+        borderRadius: 15,
     },
-    clubtypecomponentText:{
-        textAlign:'center',
-        fontSize:15,
-        fontWeight:'bold',
+    clubtypecomponentText: {
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: 'bold',
     },
-    clubtextView:{
-        width:'100%',
-        height:'20%',
-        alignItems:'center',
-        justifyContent:'center',
+    clubtextView: {
+        width: '100%',
+        height: '20%',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 
 
@@ -61,54 +61,54 @@ const styles = StyleSheet.create({
 
     // -----------------ClubTypeAndActivityLogScreenComponent
     //  분괍 별로 나누어진거, 선, 나의 동아리 활동일지 페이지 컴포넌트
-    clubsAndTypeContainer:{ // 분과 버튼
-        width : width,
-        height : height,
-        alignItems:'center',
+    clubsAndTypeContainer: { // 분과 버튼
+        width: width,
+        height: height,
+        alignItems: 'center',
     },
-    clubsView:{
-        width:'85%',
-        flexWrap:'wrap',
-        flexDirection:'row',
+    clubsView: {
+        width: '85%',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
     },
-    line:{
-        backgroundColor:'#D9D9D9', 
-        height:2, 
-        width:'85%',
-        marginTop:30,
-        marginBottom:20,
+    line: {
+        backgroundColor: '#D9D9D9',
+        height: 2,
+        width: '85%',
+        marginTop: 30,
+        marginBottom: 20,
     },
-    myActivityLogTextView:{
-        width:'85%',
-        marginBotttom:20,
-        padding:10,
+    myActivityLogTextView: {
+        width: '85%',
+        marginBotttom: 20,
+        padding: 10,
     },
-    myActivityLogText:{
-        textAlign:'left',
-        fontSize:20,
-        fontWeight:'bold',
+    myActivityLogText: {
+        textAlign: 'left',
+        fontSize: 20,
+        fontWeight: 'bold',
     },
-    myActivityLogView:{ // 활동일지 
-        width:'85%',
-        height:height*0.5,
-        borderRadius:15,
-        borderWidth:2,
-        justifyContent:'center',
-        alignItems:'center',
+    myActivityLogView: { // 활동일지 
+        width: '85%',
+        height: height * 0.5,
+        borderRadius: 15,
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    ActivityComponentView:{
-        width:'85%',
-        height:'90%',
-        justifyContent:'center',
-        alignItems:'center',
-        paddingTop:10,
+    ActivityComponentView: {
+        width: '85%',
+        height: '90%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 10,
     },
-    myActivityLogmoreView:{
-        flexDirection:'row', 
-        width:'95%',
-        height:'10%',
-        justifyContent:'flex-end',
-        alignItems:'center',
+    myActivityLogmoreView: {
+        flexDirection: 'row',
+        width: '95%',
+        height: '10%',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
     },
 
 
@@ -116,120 +116,120 @@ const styles = StyleSheet.create({
 
     //----------ActivitiLogComponent
     // 활동 일지 컴포넌트
-    activityLogComponentView:{
-        width:'100%',
-        height:height*0.13,
-        flexDirection:'row',
-        marginBottom:10,
+    activityLogComponentView: {
+        width: '100%',
+        height: height * 0.13,
+        flexDirection: 'row',
+        marginBottom: 10,
     },
-    activityLogImageView:{
-        width:'20%',
-        height:'100%',
-        justifyContent:'center',
+    activityLogImageView: {
+        width: '20%',
+        height: '100%',
+        justifyContent: 'center',
     },
-    activityLogTextView:{
-        width:'75%',
-        height:'100%',
-        paddingLeft:10,
-        paddingRight:10,
+    activityLogTextView: {
+        width: '75%',
+        height: '100%',
+        paddingLeft: 10,
+        paddingRight: 10,
     },
-    activityLogClubName:{
-        fontSize:13,
-        marginTop:8,
+    activityLogClubName: {
+        fontSize: 13,
+        marginTop: 8,
     },
-    activityLogTitleText:{
-        fontSize:18,
-        fontWeight:'bold',
-        marginTop:8,
+    activityLogTitleText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 8,
     },
-    activityLogContents:{
-        fontSize:15,
-        marginTop:8,
+    activityLogContents: {
+        fontSize: 15,
+        marginTop: 8,
     },
-    activityLogMoreImage:{
-        width:'5%',
+    activityLogMoreImage: {
+        width: '5%',
     },
 
 
 
     // -----------------AnnouncementListScreenComponent
     // 모집 공고 리스트
-    announcementListContainer:{
-        width:'85%',
-        height:'100%',
-        alignItems:'center',
+    announcementListContainer: {
+        width: '85%',
+        height: '100%',
+        alignItems: 'center',
     },
-    searchView:{ // 검색창
-        width:'85%',
-        height:'5%',
-        marginTop:15,
-        marginBottom:25,
-        flexDirection:'row',
-        backgroundColor:'white',
-        justifyContent:'center',
-        alignItems:'center',
+    searchView: { // 검색창
+        width: '85%',
+        height: '5%',
+        marginTop: 15,
+        marginBottom: 25,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    searchText:{
-        width:'80%',
-        height:'100%',
-        borderRadius:15,
-        padding:5,
-        paddingLeft:10,
-        marginRight:15,
-        fontSize:15,
-        backgroundColor:'#D9D9D9',
+    searchText: {
+        width: '80%',
+        height: '100%',
+        borderRadius: 15,
+        padding: 5,
+        paddingLeft: 10,
+        marginRight: 15,
+        fontSize: 15,
+        backgroundColor: '#D9D9D9',
     },
-    searchIcon:{
-        width:'20%',
-        height:'100%',
-        borderRadius: 15, 
-        justifyContent:'center',
-        alignItems:'center',        
-        backgroundColor:'#D9D9D9',
+    searchIcon: {
+        width: '20%',
+        height: '100%',
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#D9D9D9',
     },
 
 
     // -----------------NotificationComponent
     // 공고 컴포넌트
-    notificationComponentView:{
-        width:'85%',
-        height:height*0.18,
-        marginBottom:5,
+    notificationComponentView: {
+        width: '85%',
+        height: height * 0.18,
+        marginBottom: 5,
     },
-    notificationClubNameView:{
-        width:'100%', 
-        height:'15%', 
-        flexDirection:'row', 
-        alignItems:'center',
-        marginBottom:5,
+    notificationClubNameView: {
+        width: '100%',
+        height: '15%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5,
     },
-    notificationTitleView:{
-        width:'100%', 
-        height:'20%', 
-        flexDirection:'row',
-        alignItems:'center', 
-        marginBottom:5,
+    notificationTitleView: {
+        width: '100%',
+        height: '20%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5,
     },
-    notificationContentsView:{
-        width:'100%', 
-        height:'30%', 
-        marginBottom:5,
+    notificationContentsView: {
+        width: '100%',
+        height: '30%',
+        marginBottom: 5,
     },
-    notificationInfoView:{
-        width:'100%', 
-        height:'20%', 
-        flexDirection:'row', 
-        marginBottom:10,
-        justifyContent:'space-between',
-        alignItems:'center',
-        paddingHorizontal:10,        
-    }, 
-    Line:{
-        width:'85%', 
-        borderWidth:1, 
-        borderColor:'#ECECEC', 
-        marginTop:15,
-        marginBottom:15,
+    notificationInfoView: {
+        width: '100%',
+        height: '20%',
+        flexDirection: 'row',
+        marginBottom: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+    },
+    Line: {
+        width: '85%',
+        borderWidth: 1,
+        borderColor: '#ECECEC',
+        marginTop: 15,
+        marginBottom: 15,
     },
 
 
@@ -237,34 +237,42 @@ const styles = StyleSheet.create({
 
     // -----------------ClubListScreenComponent
     // 동아리 목록 screen 컴포넌트
-    clubListContainer:{
-        width : width,
-        height : height,
-        alignItems:'center',
+    clubListContainer: {
+        width: width,
+        height: height,
+        alignItems: 'center',
     },
-    clubListOption:{
-        width:'85%',
-        height:'5%', 
-        flexDirection:'row',
-        alignItems:'center',
-        marginTop:20,
+    clubListOption: {
+        width: '85%',
+        height: '5%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
     },
-    clubListPicker:{
-        width:100,
-        height:'90%',
-        marginRight:20,
-        backgroundColor:'#D9D9D9',
-        borderRadius:15,
-        justifyContent:'center',
-        alignItems:'center',
+    clubListPicker: {
+        width: 100,
+        height: '90%',
+        marginRight: 20,
+        backgroundColor: '#D9D9D9',
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    recruitmentButton:{
-        width:100,
-        height:'90%',
-        backgroundColor:'#D9D9D9',
-        borderRadius:15,
-        justifyContent:'center',
-        alignItems:'center',
+    recruitmentButton: {
+        width: 100,
+        height: '90%',
+        backgroundColor: '#D9D9D9',
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalview: {
+        width: width * 0.8,
+        height: height * 0.5,
+        backgroundColor: 'pink',
+        borderRadius: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     inputModal: {
         height: 55,
@@ -282,46 +290,46 @@ const styles = StyleSheet.create({
 
     // ---------------clubComponent
     // 동아리 컴포넌트
-    ClubComponentView:{
-        width:'85%',
-        height:height*0.15,
-        marginBottom:10,
-        flexDirection:'row',
-        alignItems:'center',
+    ClubComponentView: {
+        width: '85%',
+        height: height * 0.15,
+        marginBottom: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
     },
-    clubmark:{
-        width:'30%',
-        height:'100%',
-        justifyContent:'center',
-        alignItems:'center',
+    clubmark: {
+        width: '30%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    clubcontent:{
-        width:'55%',
-        height:'80%',
-    },  
-    clubTypeText:{
-        fontSize:15,
-        marginTop:8,
-        color:'#138CDB'
+    clubcontent: {
+        width: '55%',
+        height: '80%',
     },
-    clubName:{
-        fontSize:25,
-        marginTop:8,
-        fontWeight:'bold',
+    clubTypeText: {
+        fontSize: 15,
+        marginTop: 8,
+        color: '#138CDB'
     },
-    clubisrecruit:{
-        width:'15%',
-        height:'30%',
-        backgroundColor:'pink',
-        borderRadius:15,
-        justifyContent:'center',
+    clubName: {
+        fontSize: 25,
+        marginTop: 8,
+        fontWeight: 'bold',
     },
-    isrecruitText:{
-        fontSize:13,
-        fontWeight:'bold',
-        color:'white',
-        textAlign:'center',
-        textAlignVertical:'center',
+    clubisrecruit: {
+        width: '15%',
+        height: '30%',
+        backgroundColor: 'pink',
+        borderRadius: 15,
+        justifyContent: 'center',
+    },
+    isrecruitText: {
+        fontSize: 13,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+        textAlignVertical: 'center',
     },
 
 
@@ -338,23 +346,23 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
     },
-    clublistisadmin:{
-        width:'15%',
-        height:'30%',
-        borderRadius:15,
-        alignItems:'center',
-        justifyContent:'center',
+    clublistisadmin: {
+        width: '15%',
+        height: '30%',
+        borderRadius: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    clubCreateButtonView:{
-        position:'absolute',
+    clubCreateButtonView: {
+        position: 'absolute',
         bottom: 10,
         right: 10,
-        width:50, 
-        height:50, 
-        backgroundColor:'#D9D9D9',
-        borderRadius:40, 
-        justifyContent:'center', 
-        alignItems:'center',
+        width: 50,
+        height: 50,
+        backgroundColor: '#D9D9D9',
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 
 
@@ -364,26 +372,32 @@ const styles = StyleSheet.create({
 
     // -----------------ClubCreationScreen
     // 동아리 생성 페이지
-    clubcreateContainer:{
-        width : width,
-        height : height,
-        backgroundColor:'white',
-        alignItems:'center',
+    clubcreateContainer: {
+        width: width,
+        height: height,
+        backgroundColor: 'white',
+        alignItems: 'center',
     },
-    clubcreateView:{
-        width:'85%',
-        height:'100%',
+    clubcreateView: {
+        width: '85%',
+        height: '100%',
     },
-    createClubText:{
-        fontSize:20,
-        fontWeight:'bold',
-        marginTop:20,
-        marginBottom:10,
+    createClubText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginBottom: 10,
     },
-    clubcreateTextInput:{
-        borderBottomWidth:1,
-        borderColor:'#D9D9D9',
-        padding:10,
+    clubcreateTextInput: {
+        borderBottomWidth: 1,
+        borderColor: '#D9D9D9',
+        padding: 10,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
     },
     createClubLeaderInfoView:{
         width:330,
@@ -394,6 +408,69 @@ const styles = StyleSheet.create({
         padding:10,
         paddingHorizontal:20,
     },
+    modalView: {
+        width: width*0.85,
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    modalTopView:{
+        width: '100%',
+        height: '10%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 15,
+    },
+    modalCloseView:{
+        width: '15%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalCloseButton:{
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        backgroundColor: '#D9D9D9',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalSearchView:{
+        width: 230,
+        height: 40,
+        marginTop: 15,
+        marginBottom: 25,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    totaluserinfoView:{
+        width: '85%',
+        height: 300,
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: '#D9D9D9',
+        borderRadius: 15,
+    },
+    userinfoView:{
+        width: '100%',
+        borderBottomWidth:1,
+        borderColor:'#D9D9D9',
+        justifyContent:'center',
+    },
+    userinfonameText:{
+
+    }
 });
 
 
