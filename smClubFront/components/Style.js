07d60db20e239e56b17a9a0da0876e86b577e544
yeonji123 <duathshdwkd1@naver.com>
2023-08-13 20:31:03 +0900

    🐛Fix) front) change style code
    get back to the original state

--- smClubFront/components/Style.js
+++ smClubFront/components/Style.js
@@ -1,13 +1,20 @@
 // 모든 페이지들의 스타일 파일
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#fff',
-        alignItems:'center',
-        justifyContent:'center',
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+    containerView: {
+        width: width,
+        height: height,
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
 
-    }
 });
 export default styles;
