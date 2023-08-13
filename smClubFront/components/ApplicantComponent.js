a49d88437430016586176bf89792e2a3dff0ba5f
yeonji123 <duathshdwkd1@naver.com>
2023-08-13 20:25:26 +0900

    🐛Fix) front) fix div error

--- smClubFront/components/ApplicantComponent.js
+++ smClubFront/components/ApplicantComponent.js
@@ -1,11 +1,13 @@
 // 동아리 가입 신청자 컴포넌트
 import React from 'react';
-
+import {
+    View
+} from 'react-native';
 const ApplicateComponent = () => {
     return (
-        <div>
+        <View>
             
-        </div>
+        </View>
     );
 };
 
