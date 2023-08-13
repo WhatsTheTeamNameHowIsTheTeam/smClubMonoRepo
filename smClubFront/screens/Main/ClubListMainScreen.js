57a71c0ed000fa9d05190bc0ca4c836d5f084451
yeonji123 <duathshdwkd1@naver.com>
2023-08-13 20:18:21 +0900

    📝Move) front) move file mainPage folder
    and clublistMainScreen.js file

---
+++ smClubFront/screens/Main/ClubListMainScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    View,
+} from 'react-native';
+
+
+const ClubListMainScreen = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+export default ClubListMainScreen;
