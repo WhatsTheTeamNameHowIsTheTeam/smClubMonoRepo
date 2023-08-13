f89de68567d725d7cf82d891006317c3145873e6
yeonji123 <duathshdwkd1@naver.com>
2023-08-13 20:20:41 +0900

    📝Produce) front) create unioncliblistScreen and component file for union admin

---
+++ smClubFront/components/ClubComponent/UnionClubListScreenComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    View
+} from 'react-native';
+const UnionClubListScreenComponent = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+export default UnionClubListScreenComponent;
