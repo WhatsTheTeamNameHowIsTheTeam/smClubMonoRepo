b0ce2e3a0453b251b8ef51ef5e5c8b2685e93d58
yeonji123 <duathshdwkd1@naver.com>
2023-08-16 03:40:45 +0900

    💄Design) front) design view layout

--- smClubFront/screens/MyPage/MyPageScreen.js
+++ smClubFront/screens/MyPage/MyPageScreen.js
@@ -1,11 +1,13 @@
 //마이페이지
 // 모듈
 import React from 'react';
-import { View, Button, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import {
+    View, Image, Text,
+    StyleSheet, TouchableOpacity,
+    Alert, Dimensions
+} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// 파일
-import styles from '../../components/Style'
 
 const MyPageScreen = ({ navigation }) => {
 
@@ -43,134 +45,144 @@ const MyPageScreen = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-
-            {/* 내정보 */}
-            <View style={MyPagestyles.myInfo} >
-                <View style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    marginRight: 10
-                }}>
-                    <TouchableOpacity>
-                        <View style={MyPagestyles.profile}></View>
-                    </TouchableOpacity>
+            <View style={styles.myPageContainer}>
+                <View style={styles.profileView}>
+                    <View style={styles.profile}>
+                        <View style={{ width: '40%', height: '85%', }}>
+                            <Image style={{ width: '100%', height: '100%' }} source={require('../../assets/favicon.png')} resizeMethod='resize' />
+                        </View>
+                        <View style={{ width: '60%', height: '85%', justifyContent: 'center', paddingLeft: 15, }}>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 3 }}>Name</Text>
+                            <Text style={{ fontSize: 18, marginBottom: 3 }}>학과</Text>
+                            <Text style={{ fontSize: 18, marginBottom: 3 }}>학번</Text>
+                            <Text style={{ fontSize: 18, marginBottom: 3 }}>전화번호</Text>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: 'row', height: '20%', width: '90%', justifyContent: 'flex-end', alignItems: 'center' }}>
+                        <Text style={{ fontSize: 15, }}>프로필 수정</Text>
+                        <Image style={{ width: 20, height: 20 }} source={require('../../assets/ArrowRightShort.png')} />
+                    </View>
                 </View>
 
-                <View style={{
-                    flexDirection: 'column',
-                    flex: 2,
-                    justifyContent: 'center'
-                }}>
-                    <Text style={{ fontSize: 20, marginBottom: 8, }}>김세현</Text>
-                    <Text style={{ marginBottom: 5, }}>신소재공학과</Text>
-                    <Text>010-2232-2920</Text>
+                <View style={styles.functionView}>
+                    <View style={styles.subView}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 23 }}>동아리</Text>
+                        {/* 동아리 목록 */}
+                        <TouchableOpacity
+                            style={styles.setup}
+                            onPress={() => {
+
+                            }}>
+                            <Text style={{ fontSize: 20 }}>동아리 목록</Text>
+                        </TouchableOpacity>
+
+
+                        {/* My 동아리 활동 일지 */}
+                        <TouchableOpacity
+                            style={styles.setup}
+                            onPress={() => {
+
+                            }}>
+                            <Text style={{ fontSize: 20 }}>My 동아리 활동 일지</Text>
+                        </TouchableOpacity>
+
+
+                        {/* 동아리 가입 신청 내역 */}
+                        <TouchableOpacity
+                            style={styles.setup}
+                            onPress={() => {
+
+                            }}>
+                            <Text style={{ fontSize: 20 }}>동아리 가입 신청 내역</Text>
+                        </TouchableOpacity>
+                    </View>
+
+
+                    <View style={styles.subView}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 23 }}>기타</Text>
+                        {/* 약관 및 정책 */}
+                        <TouchableOpacity
+                            style={styles.setup}
+                            onPress={() => {
+
+                            }}>
+                            <Text style={{ fontSize: 20 }}>약관 및 정책</Text>
+                        </TouchableOpacity>
+
+
+
+                        {/* 로그아웃 */}
+                        <TouchableOpacity
+                            style={styles.setup}
+                            onPress={() => {
+                                logoutButtonPress();
+                            }}>
+                            <Text style={{ fontSize: 20 }}>로그아웃</Text>
+                        </TouchableOpacity>
+
+
+                        {/* 탈퇴하기 */}
+                        <TouchableOpacity
+                            style={styles.setup}
+                            onPress={() => {
+                                secessionButtonPress();
+                            }}>
+                            <Text style={{ fontSize: 20 }}>탈퇴하기</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-
-            {/* 약관 및 설정 */}
-            <View style={{ flex: 2.5 }}>
-
-                <TouchableOpacity
-                    style={MyPagestyles.setup}
-                    onPress={() => {
-
-                    }}
-                >
-                    <Text style={MyPagestyles.setupText} >MY 동아리</Text>
-                    <View style={MyPagestyles.separator} />
-
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    style={MyPagestyles.setup}
-                    onPress={() => {
-
-                    }}
-                >
-                    <Text style={MyPagestyles.setupText}>MY 동아리 활동 일지</Text>
-                    <View style={MyPagestyles.separator} />
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    style={MyPagestyles.setup}
-                    onPress={() => {
-
-                    }}
-                >
-                    <Text style={MyPagestyles.setupText}>동아리 가입 신청 내역</Text>
-                    <View style={MyPagestyles.separator} />
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    style={MyPagestyles.setup}
-                    onPress={() => {
-
-                    }}
-                >
-                    <Text style={MyPagestyles.setupText}>약관 및 정책</Text>
-                    <View style={MyPagestyles.separator} />
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    style={MyPagestyles.setup}
-                    onPress={() => {
-
-                    }}
-                >
-                    <Text
-                        style={MyPagestyles.setupText}
-                        onPress={secessionButtonPress}
-                    >
-                        어플 탈퇴하기
-                    </Text>
-                    <View style={MyPagestyles.separator} />
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    style={MyPagestyles.setup}
-                    onPress={() => {
-
-                    }}
-                >
-                    <Text
-                        style={MyPagestyles.setupText}
-                        onPress={logoutButtonPress}
-                    >
-                        로그아웃
-                    </Text>
-                    <View style={MyPagestyles.separator} />
-                </TouchableOpacity>
-            </View>
-
         </View>
     );
 };
-const MyPagestyles = StyleSheet.create({
-    myInfo: {
+const styles = StyleSheet.create({
+    container: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+        backgroundColor: '#fff',
+    },
+    myPageContainer: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+    },
+    profileView: {
+        width: '90%',
+        height: '20%',
         backgroundColor: '#D9D9D9',
-        borderRadius: 10,
-        alignContent: 'center',
-        flexDirection: 'row',
-        marginBottom: 20,
-        padding: '7%',
+        borderRadius: 15,
+        marginTop: 20,
+        alignItems: 'center',
     },
     profile: {
-        backgroundColor: 'white',
-        width: 90,
-        height: 90,
-        borderRadius: 10,
+        flexDirection: 'row',
+        height: '80%',
+        width: '80%',
+        alignItems: 'flex-end'
     },
-    separator: {
-        borderBottomColor: 'black',
-        borderBottomWidth: StyleSheet.hairlineWidth,
+    modifyProfile: {
+        flexDirection: 'row',
+        height: '20%',
+        width: '90%',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    functionView: {
+        width: '90%',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    subView: {
+        width: '100%',
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#D9D9D9',
+        padding: 15,
+        marginBottom: 20,
     },
     setup: {
         width: '100%',
         marginTop: 15,
-        justifyContent: 'flex-start',
-    },
-    setupText: {
-        fontSize: 30,
     },
 });
 
