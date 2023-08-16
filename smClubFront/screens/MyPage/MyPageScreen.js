ee5bee4636c0885b4094ad5ec8d885cf65d2d21d
yeonji123 <duathshdwkd1@naver.com>
2023-08-16 17:41:44 +0900

    ✨feat) front) myPage layout and function complete

--- smClubFront/screens/MyPage/MyPageScreen.js
+++ smClubFront/screens/MyPage/MyPageScreen.js
@@ -1,15 +1,133 @@
 //마이페이지
 // 모듈
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    View, Image, Text,
+    View, Image, Text, TextInput,
     StyleSheet, TouchableOpacity,
-    Alert, Dimensions
+    Alert, Dimensions, Modal,
+    Pressable, Keyboard,
 } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// npx expo install expo-image-picker
+import * as ImagePicker from 'expo-image-picker';
+// npm install react-native-dropdown-picker
+import DropDownPicker from 'react-native-dropdown-picker';
 
+// -------Component
+import Storage from '../../components/Storage';
+
+// -------Data
+import Majors from '../../Data/majors.json'
 
 const MyPageScreen = ({ navigation }) => {
+    const [name, setName] = useState("이름이름"); // 이름
+    const [major, setMajor] = useState("컴공"); // 학과
+    const [studentID, setStudentID] = useState(1234); // 학번
+    const [phone, setPhone] = useState(1234); // 전화번호
+    const [errorMessageName, setErrorMessageName] = useState(""); // 이름 에러메세지
+    const [errorMessageMajor, setErrorMessageMajor] = useState(""); // 학과 에러메세지
+    const [errorMessagePhone, setErrorMessagePhone] = useState(""); // 전화번호 에러메세지
+    const [isModalVisible, setIsModalVisible] = useState(false); // 모달
+
+    const [modifyType, setModifyType] = useState(""); // 수정 타입
+
+    // select box
+    const [open, setOpen] = useState(false);
+    const [majors, setMajors] = useState(''); // 동아리 종류
+    const [items, setItems] = useState(Majors.majors);
+
+    // 프로필 수정
+    const modifyProfile = () => {
+        Alert.alert('프로필 수정', '프로필을 수정하시겠습니까?', [
+            {
+                text: '이름 수정',
+                onPress: () => {
+                    setIsModalVisible(!isModalVisible);
+                    setModifyType("이름");
+                }
+            },
+            {
+                text: '학과 수정',
+                onPress: () => {
+                    setIsModalVisible(!isModalVisible);
+                    setModifyType("학과");
+                }
+            },
+            {
+                text: '전화번호 수정',
+                onPress: () => {
+                    setIsModalVisible(!isModalVisible);
+                    setModifyType("전화번호");
+                }
+            },
+            {
+                text: '취소',
+                style: 'cancel',
+            },
+
+        ])
+    }
+
+
+    //이름 정규식
+    const validateName = name => {
+        const regex = /^[ㄱ-힣]{1,20}$/;
+        return regex.test(name);
+    }
+    //전화번호 정규식
+    const validatePhone = phone => {
+        const regex = /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$/;
+        return regex.test(phone);
+    }
+    // 자동 하이픈 생성
+    const autoHyphen = (target) => {
+        return target.replace(/[^0-9]/g, '').replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/(\-{1,2})$/g, "");
+    }
+    //이름 핸들러
+    const handleNameChange = (name) => {
+        setName(name);
+        setErrorMessageName(
+            validateName(name) ? "올바른 이름 형식입니다." : "이름을 올바르게 입력해주세요."
+        );
+    }
+    //전화번호 핸들러
+    const handlePhoneChange = (call) => {
+        const changedPhone = autoHyphen(call);
+        setPhone(changedPhone);
+        setErrorMessagePhone(
+            validatePhone(changedPhone) ? "올바른 휴대전화 번호입니다" : "올바른 휴대전화 번호가 아닙니다."
+        );
+    }
+
+
+    // 프로필 이미지
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setEditMode(true)
+            setProfileImage(result.assets[0].uri); // 선택한 이미지의 URI를 상태로 설정
+        }
+    };
+
+    // 서버로 값 보내기
+    const sendServer = (func)=>{
+        if (func==='profile'){
+            // 프로필 수정
+            var object = {
+
+            }
+
+        } else if (func === 'secession') {
+            // 탈퇴
+
+
+        }
+    }
 
     //탈퇴 확인
     const secessionButtonPress = () => {
@@ -18,7 +136,7 @@ const MyPageScreen = ({ navigation }) => {
                 text: '취소',
                 style: 'cancel',
             },
-            { text: '확인' },
+            { text: '확인', onPress: () => { logout(); } },
         ]);
     }
 
@@ -36,7 +154,7 @@ const MyPageScreen = ({ navigation }) => {
     // 로그아웃
     const logout = async () => {
         try {
-            AsyncStorage.clear();
+            // Storage.removeData("id")
             navigation.navigate('LoginScreen');
         } catch (error) {
             console.log(error);
@@ -46,22 +164,130 @@ const MyPageScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <View style={styles.myPageContainer}>
+
+                <View style={{ justifyContent: 'center', alignItems: 'center', }}>
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={isModalVisible}
+                        onRequestClose={() => {
+                            setIsModalVisible(false)
+                        }}>
+                        <Pressable style={styles.centeredView} onPress={() => { Keyboard.dismiss() }}>
+                            <View style={styles.modalView}>
+                                {/* 닫기 */}
+                                <View style={styles.modalTopView}>
+                                    <View style={styles.modalCloseView}></View>
+                                    <Text style={{ fontSize: 20, fontWeight: 'bold' }} onPress={() => { setIsModalVisible(false); }}>회원 정보</Text>
+                                    <View style={styles.modalCloseView}>
+                                        <TouchableOpacity
+                                            style={styles.modalCloseButton}
+                                            onPress={() => {
+                                                setIsModalVisible(false);
+                                            }}>
+                                            <Text style={{ fontSize: 20 }}>X</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+
+
+                                <View style={modifyType==="학과"?[styles.modalMidView, {height:280}]:styles.modalMidView}>
+                                    {
+                                        modifyType === "이름" ?
+                                            <>
+                                                <Text>* 수정 하실 이름을 작성해주세요</Text>
+                                                <TextInput
+                                                    style={styles.modalTextInput}
+                                                    value={name}
+                                                    onChangeText={handleNameChange}
+                                                    placeholder="Name"
+                                                />
+                                                <Text style={{color:'red', fontSize:12}}>{errorMessageName}</Text>
+                                            </>
+                                            :
+                                            <>
+                                                {
+                                                    modifyType === "학과" ?
+                                                        <>
+                                                            <Text>* 수정 하실 학과를 골라주세요</Text>
+                                                            
+                                                            <DropDownPicker
+                                                                style={{ width: '100%', marginTop: 10,}}
+                                                                open={open}
+                                                                value={majors}
+                                                                items={items}
+                                                                setOpen={setOpen}
+                                                                setValue={setMajors}
+                                                                setItems={setItems}
+                                                            />
+                                                            <Text style={{color:'red', fontSize:12}}>{errorMessageMajor}</Text>
+                                                        </>
+                                                    :
+                                                    <>
+                                                            {
+                                                                modifyType === "전화번호" ?
+                                                                    <>
+                                                                        <Text>* 수정할 전화번호를 입력해주세요</Text>
+                                                                        <TextInput
+                                                                            style={styles.modalTextInput}
+                                                                            value={phone}
+                                                                            onChangeText={handlePhoneChange}
+                                                                            placeholder="number"
+                                                                            keyboardType='numeric'
+                                                                        />
+                                                                        <Text style={{color:'red', fontSize:12}}>{errorMessagePhone}</Text>
+                                                                    </>
+                                                                    :
+                                                                    null
+                                                            }
+                                                    </>
+                                            }</>
+                                    }
+
+
+
+                                </View>
+
+                                <TouchableOpacity
+                                    style={styles.modalSaveView}
+                                    onPress={() => {
+                                        // 서버에 저장
+                                        sendServer('profile') // 프로필 수정
+                                        setIsModalVisible(false)
+                                    }}>
+                                    <Text style={{ fontSize: 15 }}>저장</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </Pressable>
+                    </Modal>
+                </View>
+
                 <View style={styles.profileView}>
                     <View style={styles.profile}>
-                        <View style={{ width: '40%', height: '85%', }}>
+                        <TouchableOpacity
+                            style={{ width: '40%', height: '85%', }}
+                            onPress={() => {
+                                pickImage();
+                            }}
+                        >
                             <Image style={{ width: '100%', height: '100%' }} source={require('../../assets/favicon.png')} resizeMethod='resize' />
-                        </View>
+                        </TouchableOpacity>
                         <View style={{ width: '60%', height: '85%', justifyContent: 'center', paddingLeft: 15, }}>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 3 }}>Name</Text>
-                            <Text style={{ fontSize: 18, marginBottom: 3 }}>학과</Text>
-                            <Text style={{ fontSize: 18, marginBottom: 3 }}>학번</Text>
-                            <Text style={{ fontSize: 18, marginBottom: 3 }}>전화번호</Text>
+                            <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 3 }}>{name}</Text>
+                            <Text style={{ fontSize: 18, marginBottom: 3 }}>{major}</Text>
+                            <Text style={{ fontSize: 18, marginBottom: 3 }}>{studentID}</Text>
+                            <Text style={{ fontSize: 18, marginBottom: 3 }}>{phone}</Text>
                         </View>
                     </View>
-                    <View style={{ flexDirection: 'row', height: '20%', width: '90%', justifyContent: 'flex-end', alignItems: 'center' }}>
+                    <TouchableOpacity
+                        style={{ flexDirection: 'row', height: '20%', width: '90%', justifyContent: 'flex-end', alignItems: 'center' }}
+                        onPress={() => {
+                            modifyProfile()
+                        }}
+                    >
                         <Text style={{ fontSize: 15, }}>프로필 수정</Text>
                         <Image style={{ width: 20, height: 20 }} source={require('../../assets/ArrowRightShort.png')} />
-                    </View>
+                    </TouchableOpacity>
                 </View>
 
                 <View style={styles.functionView}>
@@ -71,7 +297,9 @@ const MyPageScreen = ({ navigation }) => {
                         <TouchableOpacity
                             style={styles.setup}
                             onPress={() => {
-
+                                navigation.navigate('MyClub', {
+                                    screen:'MyClubListScreen',
+                                })
                             }}>
                             <Text style={{ fontSize: 20 }}>동아리 목록</Text>
                         </TouchableOpacity>
@@ -81,7 +309,9 @@ const MyPageScreen = ({ navigation }) => {
                         <TouchableOpacity
                             style={styles.setup}
                             onPress={() => {
-
+                                navigation.navigate('MyClub', {
+                                    screen:'MyClubListScreen',
+                                })
                             }}>
                             <Text style={{ fontSize: 20 }}>My 동아리 활동 일지</Text>
                         </TouchableOpacity>
@@ -91,7 +321,9 @@ const MyPageScreen = ({ navigation }) => {
                         <TouchableOpacity
                             style={styles.setup}
                             onPress={() => {
-
+                                navigation.navigate('MyClub', {
+                                    screen:'MyClubListScreen',
+                                })
                             }}>
                             <Text style={{ fontSize: 20 }}>동아리 가입 신청 내역</Text>
                         </TouchableOpacity>
@@ -104,7 +336,7 @@ const MyPageScreen = ({ navigation }) => {
                         <TouchableOpacity
                             style={styles.setup}
                             onPress={() => {
-
+                                
                             }}>
                             <Text style={{ fontSize: 20 }}>약관 및 정책</Text>
                         </TouchableOpacity>
@@ -184,6 +416,72 @@ const styles = StyleSheet.create({
         width: '100%',
         marginTop: 15,
     },
+    modalView: {
+        width: "80%",
+        backgroundColor: "white",
+        borderRadius: 6,
+        padding: 20,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalTopView: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 15,
+    },
+    modalMidView: {
+        width: '100%',
+        marginTop: 10,
+        alignItems: 'center',
+    },
+    modalTextInput: {
+        width: '60%',
+        height: 40,
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor: '#D9D9D9',
+        marginTop: 10,
+        borderRadius: 10,
+        paddingLeft: 10,
+    },
+    modalCloseView: {
+        width: '15%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalCloseButton: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        backgroundColor: '#D9D9D9',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalSaveView: {
+        width: '20%',
+        height: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+        borderRadius: 10,
+        backgroundColor: '#D9D9D9',
+    }
+
 });
 
 export default MyPageScreen;
