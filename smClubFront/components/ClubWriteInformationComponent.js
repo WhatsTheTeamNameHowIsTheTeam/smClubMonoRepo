d2f2ce0e77ef1f9cd6280bf887199e4e40f4ac97
김세현 <60885427+sehyun00@users.noreply.github.com>
2023-08-17 04:12:29 +0900

    🎨Design) Front) add doneButton

--- smClubFront/components/ClubWriteInformationComponent.js
+++ smClubFront/components/ClubWriteInformationComponent.js
@@ -78,63 +78,69 @@ const ClubWriteInformationComponent = () => {
     };
 
     return (
-        <ScrollView>
-            {/* 동아리 메인 사진 */}
-            <TouchableOpacity
-                style={{ height: 200, marginBottom: 10 }}
-                onPress={() => selectImage(setMainImage, setUploadedMainImageData)}
-            >
-                <Image style={{ width: '100%', height: '100%' }} source={mainImage} resizeMode='contain' />
-            </TouchableOpacity>
-
-            <View style={styles.clubNameContainer}>
-                {/* 동아리 아이콘 */}
-                <TouchableOpacity onPress={() => selectImage(setClubIcon, setUploadedIconData)}>
-                    <Image style={{ width: 70, height: 70, borderRadius: 100, marginRight: 10 }} source={clubIcon} resizeMode='contain' />
+        <>
+            <ScrollView>
+                {/* 동아리 메인 사진 */}
+                <TouchableOpacity
+                    style={{ height: 200, marginBottom: 10 }}
+                    onPress={() => selectImage(setMainImage, setUploadedMainImageData)}
+                >
+                    <Image style={{ width: '100%', height: '100%' }} source={mainImage} resizeMode='contain' />
                 </TouchableOpacity>
 
-                <View style={{ flexDirection: 'column', justifyContent: 'space-around' }}>
-                    <Text style={styles.clubTypeText}>종류</Text>
-                    <Text style={styles.clubNameText}>동아리 명</Text>
+                <View style={styles.clubNameContainer}>
+                    {/* 동아리 아이콘 */}
+                    <TouchableOpacity onPress={() => selectImage(setClubIcon, setUploadedIconData)}>
+                        <Image style={{ width: 70, height: 70, borderRadius: 100, marginRight: 10 }} source={clubIcon} resizeMode='contain' />
+                    </TouchableOpacity>
+
+                    <View style={{ flexDirection: 'column', justifyContent: 'space-around' }}>
+                        <Text style={styles.clubTypeText}>종류</Text>
+                        <Text style={styles.clubNameText}>동아리 명</Text>
+                    </View>
                 </View>
-            </View>
 
-            {/* 동아리 홍보 내용 */}
-            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 2 }}>
-                <RichEditor
-                    ref={richText}
-                    onChange={descriptionText => {
-                        console.log("descriptionText:", descriptionText);
+                {/* 동아리 홍보 내용 */}
+                <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 2 }}>
+                    <RichEditor
+                        ref={richText}
+                        onChange={descriptionText => {
+                            console.log("descriptionText:", descriptionText);
+                        }}
+                    />
+                </KeyboardAvoidingView>
+
+                {/* 텍스트 에디터 */}
+                <RichToolbar
+                    editor={richText}
+                    actions={[
+                        actions.setBold,
+                        actions.setItalic,
+                        actions.insertBulletsList,
+                        actions.insertOrderedList,
+                        actions.setUnderline,
+                        actions.heading1,
+                        actions.heading2,
+                        actions.heading3,
+                    ]}
+                    iconMap={{
+                        [actions.heading1]: handleHead1,
+                        [actions.heading2]: handleHead2,
+                        [actions.heading3]: handleHead3
                     }}
-                />
-            </KeyboardAvoidingView>
+                >
+                </RichToolbar>
 
-            {/* 텍스트 에디터 */}
-            <RichToolbar
-                editor={richText}
-                actions={[
-                    actions.setBold,
-                    actions.setItalic,
-                    actions.insertBulletsList,
-                    actions.insertOrderedList,
-                    actions.setUnderline,
-                    actions.heading1,
-                    actions.heading2,
-                    actions.heading3,
-                ]}
-                iconMap={{
-                    [actions.heading1]: handleHead1,
-                    [actions.heading2]: handleHead2,
-                    [actions.heading3]: handleHead3
-                }}
-            >
-            </RichToolbar>
 
+            </ScrollView>
             {/* 저장 버튼 */}
-            <TouchableOpacity onPress={onSaveButtonClick}>
+            <TouchableOpacity
+                onPress={onSaveButtonClick}
+                style={styles.doneButton}
+            >
                 <Text>저장하기</Text>
             </TouchableOpacity>
-        </ScrollView>
+        </>
     );
 };
 
@@ -149,6 +155,21 @@ const styles = StyleSheet.create({
     clubNameText: {
         fontSize: 22,
         fontWeight: 'bold'
+    },
+    doneButton: {
+        padding: 16,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 25,
+        marginLeft: '56%',
+        backgroundColor: '#8BFF97',
+        borderRadius: 100,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 2,
+        width: 150,
     },
 });
 
